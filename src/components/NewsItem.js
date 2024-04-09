3bb776adf1f6bc9d486b2b4b714497448bc5cf5f
Mahesh-Motale77 <motalemahesh77@gmail.com>
2024-04-10 00:15:42 +0530

    Added author, date and source

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-        let {title, description, imgURL} = this.props;
+        let { title, description, imgURL, author, date, source} = this.props;
         return (
             <div>
                 <div className="card">
-                    <img src={imgURL?imgURL:"https://static.toiimg.com/thumb/msid-108982071,width-1070,height-580,imgsize-24700,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg"} className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">{title}...</h5>
-                            <p className="card-text">{description}...</p>
-                            <a href="#" className="btn btn-sm btn-secondary">Read More...</a>
-                        </div>
+                    <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '85%', zIndex:'1'}}>
+                        {source}
+                    </span>
+                    <img src={imgURL ? imgURL : "https://static.toiimg.com/thumb/msid-108982071,width-1070,height-580,imgsize-24700,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg"} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}...</h5>
+                        <p className="card-text">{description}...</p>
+                        <p class="card-text"><small class="text-body-secondary"> by {author ? author : "unknown"} on {new Date(date).toGMTString()} </small></p>
+                        <a href="#" className="btn btn-sm btn-secondary">Read More...</a>
+                    </div>
                 </div>
             </div>
         )
