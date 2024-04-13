e7e7e6c7ecd7b147d6191f7dc977adf54db10a97
Mahesh-Motale77 <motalemahesh77@gmail.com>
2024-04-13 20:16:20 +0530

    Ready to deploy v1.0

--- src/components/News.js
+++ src/components/News.js
@@ -30,13 +30,17 @@ export class News extends Component {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
-  async updateNews(){
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8d7d112609db49e5ab82e7d54f512ceb&pageSize=${this.props.pageSize}`;
+  async updateNews() {
+    const { country, category, pageSize, apiKey } = this.props;
+    const { page } = this.state;
+    // Updated the URL to include the page parameter
+    const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}&pageSize=${pageSize}&page=${page}`;
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults })
+    this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults });
   }
+  
 
   async componentDidMount() {
     this.updateNews()
@@ -60,8 +64,8 @@ export class News extends Component {
           {this.state.articles.map((element) => {
             return <div className="col-md-4" key={element.url}>
               <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imgURL={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
-            </div>
-          })}
+            </div>    
+          })}               
         </div>
 
         <div className="container d-flex justify-content-between">
